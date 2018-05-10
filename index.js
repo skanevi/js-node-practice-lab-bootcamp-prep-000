c62c7d4c705ef223e51b402c63524d709a0966a8
Susan Williams <eternalachievement@gmail.com>
2018-05-10 21:48:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,4 @@
 var name = "Susan"
 var height = "74"
 
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height)
-
-console.log(name)
+console.log(parseInt(height)+1)
